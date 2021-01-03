c86c5ec52f1ff73a31799b5f503414a1561909bf
lovac42 <lovac42@protonmail.com>
2021-01-02 19:53:58 -0500

    Ported to CCBC

--- src/edit_field_during_review/__init__.py
+++ src/edit_field_during_review/__init__.py
@@ -1,152 +1,124 @@
 # -*- coding: utf-8 -*-
+# Copyright 2020-2021 Lovac42
+# Copyright 2019-2020 Nickolay <kelciour@gmail.com>
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-"""
-Anki Add-on: Edit Field During Review
+# Note: The original file for 2.0 was missing the license. However, under the 2019 terms of agreement, any addon uploaded to AnkiWeb without a license must be under AGPL. This file was taken from AnkiWeb and ported to CCBC.
 
-Edit text in a field during review without opening the edit window
-
-Copyright: (c) 2019-2020 Nickolay Nonard <kelciour@gmail.com>
-"""
-
-from anki import hooks
-from anki.template import TemplateRenderContext
-from anki.utils import htmlToTextLine
-from aqt.reviewer import Reviewer
-from aqt import mw, gui_hooks
-from aqt.utils import tooltip
 
 import re
-import unicodedata
-import urllib.parse
+from aqt import mw
+from anki.hooks import wrap, addHook, runHook
+from aqt.editor import Editor
+from aqt.reviewer import Reviewer
 
 
-try:
-    from anki.rsbackend import NotFoundError # Anki 2.1.28+
-except:
-    class NotFoundError(Exception):
-        pass
-
-# https://github.com/ankitects/anki/blob/2.1.15/anki/template/template.py#L7
-clozeReg = r"(?si)\{\{(c)\d+::(.*?)(::(.*?))?\}\}"
-
-# https://github.com/ankitects/anki/blob/2.1.15/anki/latex.py#L21-L25
-latexRegexps = {
-    "standard": re.compile(r"\[latex\](.+?)\[/latex\]", re.DOTALL | re.IGNORECASE),
-    "expression": re.compile(r"\[\$\](.+?)\[/\$\]", re.DOTALL | re.IGNORECASE),
-    "math": re.compile(r"\[\$\$\](.+?)\[/\$\$\]", re.DOTALL | re.IGNORECASE),
+config = {
+  "tag": "div",
+  "undo": True
 }
 
-mathJaxReg = r"(?si)(\\[\[\(])(.*?)(\\[\]\)])"
 
-def safe_to_edit(text):
-    if '[sound:' in text:
-        return False
-    if re.search(clozeReg, text):
-        return False
-    if re.search(mathJaxReg, text):
-        return False
-    if any(re.search(regex, text) for regex in latexRegexps.values()):
-        return False
-    return True
+UNSAFE_TYPES = {
+    "soundReg": re.compile(r"\[sound:(.+?)\]"),
+    "clozeReg": re.compile(r"\<span class\=cloze\>"),
 
-def on_edit_filter(text, field, filter, context: TemplateRenderContext):
-    if filter != "edit":
+    # untested:
+    "mathJaxReg": re.compile(r"(?si)(\\[\[\(])(.*?)(\\[\]\)])"),
+    "latexStandard": re.compile(r"\[latex\](.+?)\[/latex\]", re.DOTALL | re.IGNORECASE),
+    "latexExpression": re.compile(r"\[\$\](.+?)\[/\$\]", re.DOTALL | re.IGNORECASE),
+    "latexMath": re.compile(r"\[\$\$\](.+?)\[/\$\$\]", re.DOTALL | re.IGNORECASE),
+}
+
+
+def editField(text, extra, context, field, fullname):
+    if any(r.search(text) for r in UNSAFE_TYPES.values()):
         return text
 
-    if not safe_to_edit(text):
-        return text
+    tag = config['tag']
 
-    config = mw.addonManager.getConfig(__name__)
-    card = context.card()
-    nid = card.nid if card is not None else ""
-    text = """<%s contenteditable="true" data-field="%s" data-nid="%s">%s</%s>""" % (config['tag'], field, nid, text, config['tag'])
-    text += """<script>"""
-    text += """
-            $("[contenteditable=true][data-field='%s']").blur(function() {
-                pycmd("ankisave#" + $(this).data("field") + "#" + $(this).data("nid") + "#" + $(this).html());
-            });
-        """ % field
-    if config['tag'] == "span":
-        text += """
-            $("[contenteditable=true][data-field='%s']").keydown(function(evt) {
-                if (evt.keyCode == 8) {
-                    evt.stopPropagation();
-                }
-            });
-        """ % field
-    text += """
-            $("[contenteditable=true][data-field='%s']").focus(function() {
-                pycmd("ankisave!speedfocus#");
-            });
-        """ % field
-    text += """</script>"""
-    return text
+    spanjs = "" if tag != "span" else """\
+$("[contenteditable=true][data-field='%s']").keydown(function(evt) {
+    if (evt.keyCode == 8){evt.stopPropagation();}
+});""" % field
 
-hooks.field_filter.append(on_edit_filter)
+    return """\
+<%s contenteditable="true" data-field="%s">%s</%s>
+<script>
+$("[contenteditable=true][data-field='%s']").blur(function() {
+  py.link("edit_field_off:" + $(this).data("field") + "#" + $(this).html());
+});
+$("[contenteditable=true][data-field='%s']").focus(function() {
+  py.link("edit_field_on:");
+});
+%s
+</script>
+""" % (
+    tag, field, text, tag,
+    field, field, spanjs
+)
 
-def mungeHTML(txt):
-    return "" if txt in ("<br>", "<div><br></div>") else txt
+addHook('fmod_edit', editField) #filter hook
+
+
 
 def saveField(note, fld, val):
-    if fld == "Tags":
-        tagsTxt = unicodedata.normalize("NFC", htmlToTextLine(val))
-        txt = mw.col.tags.canonify(mw.col.tags.split(tagsTxt))
-        field = note.tags
+    TAG_FIELD = True if fld == "Tags" else False
+
+    if TAG_FIELD:
+        old_data = note.tags
+        t = mw.col.tags.split(val)
+        new_data = mw.col.tags.canonify(t)
     else:
+        old_data = note[fld]
+
         # https://github.com/dae/anki/blob/47eab46f05c8cc169393c785f4c3f49cf1d7cca8/aqt/editor.py#L257-L263
-        txt = urllib.parse.unquote(val)
-        txt = unicodedata.normalize("NFC", txt)
-        txt = mungeHTML(txt)
+        editor = object.__new__(Editor)
+        txt = editor._filterHTML(val, localize=False)
         txt = txt.replace("\x00", "")
-        txt = mw.col.media.escapeImages(txt, unescape=True)
-        field = note[fld]
-    if field == txt:
+        new_data = mw.col.media.escapeImages(txt, unescape=True)
+
+    if old_data != new_data:
+        if config['undo']:
+            mw.checkpoint("Edited Field")
+        if TAG_FIELD:
+            note.tags = new_data
+        else:
+            note[fld] = new_data
+        note.flush()
+
+
+
+def linkHandler(rev, url, _old):
+    if url.startswith("edit_field"):
+        (cmd, arg) = url.split(":", 1)
+        if cmd == "edit_field_off":
+            # runHook("edit_field", False)
+
+            fld, val = arg.split("#", 1)
+            note = rev.card.note()
+            saveField(note, fld, val)
+            rev.card.q(reload=True)
+        else:
+            runHook("edit_field", True)
+
+            # For speed focus, to stop the timer.
+            mw.reviewer.bottom.web.eval("""\
+if (typeof autoAnswerTimeout !== 'undefined') {
+    clearTimeout(autoAnswerTimeout);
+}
+if (typeof autoAlertTimeout !== 'undefined') {
+    clearTimeout(autoAlertTimeout);
+}
+if (typeof autoAgainTimeout !== 'undefined') {
+    clearTimeout(autoAgainTimeout);
+}
+""")
+
         return
-    config = mw.addonManager.getConfig(__name__)
-    if config['undo']:
-        mw.checkpoint("Edit Field")
-    if fld == "Tags":
-        note.tags = txt
-    else:
-        note[fld] = txt
-    note.flush()
+    return _old(rev, url)
 
-def on_js_message(handled, url, context):
-    if not isinstance(context, Reviewer):
-        return handled
 
-    if url.startswith("ankisave#"):
-        fld, nid, val = url.replace("ankisave#", "").split("#", 2)
-        nid = int(nid)
-        card = context.card
-        note = card.note()
-        config = mw.addonManager.getConfig(__name__)
-        if config['debug']:
-            assert nid == note.id, "{} == {}".format(nid, note.id)
-        try:
-            # make sure note is not deleted
-            note2 = mw.col.getNote(nid)
-        except NotFoundError:
-            return True, None
-        except TypeError as e:
-            # NotFoundError if Anki < 2.1.28
-            if str(e) == "cannot unpack non-iterable NoneType object":
-                return True, None
-            raise(e)
-        # we need to reuse context.card.note() if nid == note.id
-        # as changes will be lost once we open the editor window
-        if nid != note.id:
-            note = note2
-        saveField(note, fld, val)
-        card.q(reload=True)
-        return True, None
-    elif url.startswith("ankisave!speedfocus#"):
-        mw.reviewer.bottom.web.eval("""
-            clearTimeout(autoAnswerTimeout);
-            clearTimeout(autoAlertTimeout);
-            clearTimeout(autoAgainTimeout);
-        """)
-        return True, None
-    return handled
-
-gui_hooks.webview_did_receive_js_message.append(on_js_message)
+Reviewer._linkHandler = wrap(
+    Reviewer._linkHandler, linkHandler, "around"
+)
